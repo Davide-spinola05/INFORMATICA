0e610d431aa2dd80ac270bf6d989865789d86472
Davide-spinola05 <145539424+Davide-spinola05@users.noreply.github.com>
2025-02-06 09:41:27 +0100

    Update riservata.php

--- ES04A-Sessioni/riservata.php
+++ ES04A-Sessioni/riservata.php
@@ -1,19 +1,18 @@
-<!DOCTYPE html>
-<html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Area Riservata</title>
-</head>
-<body>
-    <?php
+
+<?php
     session_start();
     if (!isset($_SESSION['username'])) {
         header("Location: login.php");
         exit();
     }
     ?>
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <h1>Benvenuto, <?php echo $_SESSION['username']; ?>!</h1>
     <p><a href="logout.php">Logout</a></p>
-</body>
+    <title>Area Riservata</title>
+</head>
 </html>
